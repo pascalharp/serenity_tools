8d81da7cf727615c977e09ba8af9352ac357cee6
Pascal H <p.harprecht@gmail.com>
2022-01-18 11:32:12 +0100

    application command quick replies

---
+++ src/interactions/application_command.rs
@@ -0,0 +1,97 @@
+use serenity::{
+    async_trait,
+    builder::CreateEmbed,
+    client::Context,
+    model::{
+        channel::Message,
+        interactions::{
+            application_command::ApplicationCommandInteraction,
+            InteractionApplicationCommandCallbackDataFlags, InteractionResponseType,
+        },
+    },
+    Result,
+};
+
+use crate::builder::CreateEmbedExt;
+
+#[async_trait]
+pub trait ApplicationCommandInteractionExt {
+    async fn create_quick_info<T: ToString + Send>(
+        &self,
+        ctx: Context,
+        kind: InteractionResponseType,
+        text: T,
+        ephemeral: bool,
+    ) -> Result<()>;
+
+    async fn create_quick_error<T: ToString + Send>(
+        &self,
+        ctx: Context,
+        kind: InteractionResponseType,
+        text: T,
+        ephemeral: bool,
+    ) -> Result<()>;
+
+    async fn edit_quick_info<T: ToString + Send>(&self, ctx: Context, text: T) -> Result<Message>;
+
+    async fn edit_error_info<T: ToString + Send>(&self, ctx: Context, text: T) -> Result<Message>;
+}
+
+#[async_trait]
+impl ApplicationCommandInteractionExt for ApplicationCommandInteraction {
+    async fn create_quick_info<T: ToString + Send>(
+        &self,
+        ctx: Context,
+        kind: InteractionResponseType,
+        text: T,
+        ephemeral: bool,
+    ) -> Result<()> {
+        self.create_interaction_response(ctx, |r| {
+            r.kind(kind);
+            r.interaction_response_data(|d| {
+                if ephemeral {
+                    d.flags(InteractionApplicationCommandCallbackDataFlags::EPHEMERAL);
+                }
+                d.add_embed(CreateEmbed::info_box(text))
+            })
+        })
+        .await
+    }
+
+    async fn create_quick_error<T: ToString + Send>(
+        &self,
+        ctx: Context,
+        kind: InteractionResponseType,
+        text: T,
+        ephemeral: bool,
+    ) -> Result<()> {
+        self.create_interaction_response(ctx, |r| {
+            r.kind(kind);
+            r.interaction_response_data(|d| {
+                if ephemeral {
+                    d.flags(InteractionApplicationCommandCallbackDataFlags::EPHEMERAL);
+                }
+                d.add_embed(CreateEmbed::error_box(text))
+            })
+        })
+        .await
+    }
+
+    async fn edit_quick_info<T: ToString + Send>(&self, ctx: Context, text: T) -> Result<Message> {
+        self.edit_original_interaction_response(ctx, |d| {
+            d.content("");
+            d.set_embeds(Vec::new());
+            d.add_embed(CreateEmbed::info_box(text))
+        })
+        .await
+    }
+
+    async fn edit_error_info<T: ToString + Send>(&self, ctx: Context, text: T) -> Result<Message> {
+        self.edit_original_interaction_response(ctx, |d| {
+            d.content("");
+            d.set_embeds(Vec::new());
+            d.add_embed(CreateEmbed::error_box(text))
+        })
+        .await
+    }
+}
