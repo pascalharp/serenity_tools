ef64e8a25bbd251e2bb904a45d138105566de0a9
Pascal Harprecht <p.harprecht@gmail.com>
2022-01-25 15:15:36 +0000

    Add quick message interaction replys

--- src/interactions/message_component.rs
+++ src/interactions/message_component.rs
@@ -1,13 +1,13 @@
 use serenity::{
     async_trait,
     client::Context,
-    model::interactions::{
-        message_component::MessageComponentInteraction, InteractionResponseType,
-    },
-    Result,
+    model::{interactions::{
+        message_component::MessageComponentInteraction, InteractionResponseType, InteractionApplicationCommandCallbackDataFlags,
+    }, channel::Message},
+    Result, builder::CreateEmbed,
 };
 
-use crate::components::{Button, ButtonParseError};
+use crate::{components::{Button, ButtonParseError}, builder::CreateEmbedExt};
 use std::result::Result as StdResult;
 
 #[async_trait]
@@ -17,6 +17,53 @@ pub trait MessageComponentInteractionExt {
     async fn deferred_update(&self, ctx: &Context) -> Result<()>;
 
     async fn deferred_reply(&self, ctx: &Context) -> Result<()>;
+
+    async fn create_quick_info<T: ToString + Send>(
+        &self,
+        ctx: &Context,
+        text: T,
+        ephemeral: bool,
+    ) -> Result<()>;
+
+    async fn create_quick_error<T: ToString + Send>(
+        &self,
+        ctx: &Context,
+        text: T,
+        ephemeral: bool,
+    ) -> Result<()>;
+
+    async fn edit_quick_info<T: ToString + Send>(&self, ctx: &Context, text: T) -> Result<Message>;
+
+    async fn edit_quick_error<T: ToString + Send>(&self, ctx: &Context, text: T)
+        -> Result<Message>;
+
+    async fn create_followup_quick_info<T: ToString + Send>(
+        &self,
+        ctx: &Context,
+        text: T,
+        ephemeral: bool,
+    ) -> Result<Message>;
+
+    async fn create_followup_quick_error<T: ToString + Send>(
+        &self,
+        ctx: &Context,
+        text: T,
+        ephemeral: bool,
+    ) -> Result<Message>;
+
+    async fn edit_followup_quick_info<T: ToString + Send>(
+        &self,
+        ctx: &Context,
+        msg: &Message,
+        text: T,
+    ) -> Result<Message>;
+
+    async fn edit_followup_quick_error<T: ToString + Send>(
+        &self,
+        ctx: &Context,
+        msg: &Message,
+        text: T,
+    ) -> Result<Message>;
 }
 
 #[async_trait]
@@ -36,5 +83,115 @@ impl MessageComponentInteractionExt for MessageComponentInteraction {
             r.kind(InteractionResponseType::DeferredChannelMessageWithSource)
         })
         .await
+    }
+
+    async fn create_quick_info<T: ToString + Send>(
+        &self,
+        ctx: &Context,
+        text: T,
+        ephemeral: bool,
+    ) -> Result<()> {
+        self.create_interaction_response(ctx, |r| {
+            r.kind(InteractionResponseType::ChannelMessageWithSource);
+            r.interaction_response_data(|d| {
+                if ephemeral {
+                    d.flags(InteractionApplicationCommandCallbackDataFlags::EPHEMERAL);
+                }
+                d.add_embed(CreateEmbed::info_box(text))
+            })
+        })
+        .await
+    }
+
+    async fn create_quick_error<T: ToString + Send>(
+        &self,
+        ctx: &Context,
+        text: T,
+        ephemeral: bool,
+    ) -> Result<()> {
+        self.create_interaction_response(ctx, |r| {
+            r.kind(InteractionResponseType::ChannelMessageWithSource);
+            r.interaction_response_data(|d| {
+                if ephemeral {
+                    d.flags(InteractionApplicationCommandCallbackDataFlags::EPHEMERAL);
+                }
+                d.add_embed(CreateEmbed::error_box(text))
+            })
+        })
+        .await
+    }
+
+    async fn edit_quick_info<T: ToString + Send>(&self, ctx: &Context, text: T) -> Result<Message> {
+        self.edit_original_interaction_response(ctx, |d| {
+            d.content("");
+            d.set_embeds(Vec::new());
+            d.components(|c| c);
+            d.add_embed(CreateEmbed::info_box(text))
+        })
+        .await
+    }
+
+    async fn edit_quick_error<T: ToString + Send>(
+        &self,
+        ctx: &Context,
+        text: T,
+    ) -> Result<Message> {
+        self.edit_original_interaction_response(ctx, |d| {
+            d.content("");
+            d.set_embeds(Vec::new());
+            d.components(|c| c);
+            d.add_embed(CreateEmbed::error_box(text))
+        })
+        .await
+    }
+
+    async fn create_followup_quick_info<T: ToString + Send>(
+        &self,
+        ctx: &Context,
+        text: T,
+        ephemeral: bool,
+    ) -> Result<Message> {
+        self.create_followup_message(ctx, |m| {
+            if ephemeral {
+                m.flags(InteractionApplicationCommandCallbackDataFlags::EPHEMERAL);
+            }
+            m.add_embed(CreateEmbed::info_box(text))
+        })
+        .await
+    }
+
+    async fn create_followup_quick_error<T: ToString + Send>(
+        &self,
+        ctx: &Context,
+        text: T,
+        ephemeral: bool,
+    ) -> Result<Message> {
+        self.create_followup_message(ctx, |m| {
+            if ephemeral {
+                m.flags(InteractionApplicationCommandCallbackDataFlags::EPHEMERAL);
+            }
+            m.add_embed(CreateEmbed::error_box(text))
+        })
+        .await
+    }
+
+    async fn edit_followup_quick_info<T: ToString + Send>(
+        &self,
+        ctx: &Context,
+        msg: &Message,
+        text: T,
+    ) -> Result<Message> {
+        self.edit_followup_message(ctx, msg, |m| m.add_embed(CreateEmbed::info_box(text)))
+            .await
+    }
+
+    async fn edit_followup_quick_error<T: ToString + Send>(
+        &self,
+        ctx: &Context,
+        msg: &Message,
+        text: T,
+    ) -> Result<Message> {
+        self.edit_followup_message(ctx, msg, |m| m.add_embed(CreateEmbed::error_box(text)))
+            .await
     }
 }
